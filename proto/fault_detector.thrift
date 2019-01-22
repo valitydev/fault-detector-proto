e9156635d9abf1bf3fd495e8e0196c4836b94b32
n.pospolita <n.pospolita@rbkmoney.com>
2019-01-22 18:15:45 +0300

    explanations

--- proto/fault_detector.thrift
+++ proto/fault_detector.thrift
@@ -14,7 +14,7 @@ enum Reliability {
 
 struct AvailabilityResponse {
     1: required ServiceId service_id
-    2: required double timeout_rate
+    2: required double timeout_rate // сколько запросов вышли за пределы среднего значения времени
     3: required double success_rate
     4: required double conversion_rate // когда-нибудь появится
     5: required Reliability reliability // если хотим положиться на рассчёты сервиса
