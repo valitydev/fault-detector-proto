d7c9dcb1e4871ccf0152ba237038b73cb7e27e27
Baikov Dmitrii <44803026+D-Baykov@users.noreply.github.com>
2019-02-15 14:45:35 +0300

    Fix fault detector proto. Change parameters and interfaces (#5)

--- proto/fault_detector.thrift
+++ proto/fault_detector.thrift
@@ -4,25 +4,28 @@ namespace java com.rbkmoney.damsel.fault_detector
 namespace erlang fault_detector
 
 typedef base.ID ServiceId
-typedef base.ID RequestId
+typedef base.ID OperationId
+typedef i64     Milliseconds
+typedef double  FailureRate
+typedef i16     TimeoutDelta
+typedef i64     OperationsCount
 
-enum Reliability {
-    BAD = 0
-    DEGRADED = 10
-    GOOD = 20
-}
+/** Ответ сервиса определения ошибок на запрос статистики для сервиса */
+struct ServiceStatistics {
 
-struct Availability {
-    1: required ServiceId service_id
-    2: required double timeout_rate // сколько запросов вышли за пределы среднего значения времени
-    3: required double success_rate // сколько запросово из общего количества завершились успешно
-    5: required Reliability reliability // если хотим положиться на рассчёты сервиса
-}
+    /** ID сервиса */
+    1: required ServiceId         service_id
+    /** Показатель частоты отказов для данного сервиса от 0 до 1, где 0 - это отсутствие ошибок, а 1 - исключительно сбойные операции */
+    2: required FailureRate       failure_rate
+    /** Общее количество операций для сервиса */
+    3: required OperationsCount   operations_count
+    /** Количество операций завершившихся с ошибкой */
+    4: required OperationsCount   error_operations_count
+    /** Количество успешных операций превысивших ожилаемое время выполнения */
+    5: required OperationsCount   overtime_operations_count
+    /** Количество операций, которые еще выполняются и превысили ожидаемое время выполнения */
+    6: required OperationsCount   hovering_opertions_count
 
-union Operation {
-    1: Start start
-    2: Finish finish
-    3: Error error
 }
 
 struct Start {
@@ -33,47 +36,55 @@ struct Finish {
     1: required base.Timestamp time_end
 }
 
-union ErrorReason {
-    1: Timeout timeout
-    2: Unavailable unavailable
-}
-
-struct Timeout {}
-struct Unavailable {}
-
 struct Error {
     1: required base.Timestamp time_end
-    2: required ErrorReason error_reason
 }
 
-// Устанавливаем какие-то из значений для disable/enable сервиса, полезная ручка
+union OperationState {
+    1: Start        start
+    2: Finish       finish
+    3: Error        error
+}
+
+struct Operation {
+    1: required OperationId      operation_id
+    2: required OperationState   state
+}
+
+/**
+* Конфигурация детектора ошибок для конкретного сервиса
+*
+* Предполагается, что временные характеристики будут заданы в мс
+**/
 struct ServiceConfig {
-    1: required Type type
-    2: required double value // Значение от 0 до 1
+
+    /** Время жизни операции в рамках сервиса (в рамках данного времени будет рассчитано среднее время выполнения операции) */
+    1: required Milliseconds operation_lifetime
+    /** Временной интервал для "скользящего окна" (время в рамках которого будут браться операции для расчета статистики) */
+    2: required Milliseconds sliding_window
+    /**
+    * Таймаут дельта. Использвется при определении операций, которые не уложились в планируемое время выполнения.
+    * Задается как десятичное число как отображение дельты в процентах. Например, если timeout_delta = 10, а среднее
+    * время выполнения 4000мс, то операции превысившие 4400мс будут считаться как сбойные (если не задан
+    * hovering_operation_error_delay больший чем 4400)
+    */
+    3: optional TimeoutDelta timeout_delta
+    /** Время, после которого операция считается зависшей. Если не задано, то будет равно среднему времени
+    * выполнения транзакции + дельта. Если задано, то после превышения данного значения операция будет считаться сбойной
+    */
+    4: optional Milliseconds hovering_operation_error_delay
+
 }
 
-enum Type {
-    TIMEOUT_RATE
-    SUCCESS_RATE
-    ALL
-}
+exception ServiceNotFoundException {}
 
 service FaultDetector {
 
-    /**
-     * Проверка доступности сервисов
-     **/
-    list<Availability> CheckAvailability(1: list<ServiceId> services)
-
-    /**
-     * Регистрация процесса операции
-     **/
-    void RegisterOperation(1: ServiceId service_id, 2: RequestId request_id, 3: Operation operation)
-
-    /**
-     * Сброс/Установка статистики сервиса.
-     * Может потенциально пригодиться для более умных сервисов, которые вкурсе "проблем"
-     **/
-    void SetServiceStatistics(1: ServiceId service_id, 2: ServiceConfig service_config)
+    /** Инициализация параметров сервиса */
+    void InitService(1: ServiceId service_id, 2: ServiceConfig service_config)
+    /** Получение статистики по сервисам */
+    list<ServiceStatistics> GetStatistics(1: list<ServiceId> services)
+    /** Регистрация операции сервиса **/
+    void RegisterOperation(1: ServiceId service_id, 2: Operation operation, 3: ServiceConfig service_config) throws (1: ServiceNotFoundException ex1)
 
 }
